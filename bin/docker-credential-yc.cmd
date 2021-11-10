7f1caef6da10a08bad893c19a011f4a5b3fb9587
Alexa6748 <73139557+Alexa6748@users.noreply.github.com>
2021-11-10 21:34:54 +0300

    Delete bin directory

--- bin/docker-credential-yc.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-rem Path to yc executable (by default from the same directory)
-IF "%$YC%"=="" SET YC=%~dp0%yc.exe
-
-rem fallback to yc from $env:Path if not set explicitly and doesn't exist in the same directory
-IF NOT EXIST %YC% SET YC=yc
-
-CALL "%YC%" --no-user-output container docker-credential %*
